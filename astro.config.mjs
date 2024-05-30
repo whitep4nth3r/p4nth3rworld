98067253ef1aa4425c3f75d0e702fafd9be32dc0
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-05-30 15:14:21 +0100

    Horrible WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import auth from "auth-astro";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth()]
+  integrations: [auth()],
+  output: "server",
+  adapter: netlify()
 });
