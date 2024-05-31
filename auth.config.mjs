7bff82bf16f8a77198906b99bda9c413786fd199
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-05-31 13:36:59 +0100

    Basic styles and session management and get inventory

--- auth.config.mjs
+++ auth.config.mjs
@@ -6,6 +6,24 @@ export default defineConfig({
     Twitch({
       clientId: import.meta.env.AUTH_TWITCH_ID,
       clientSecret: import.meta.env.AUTH_TWITCH_SECRET,
+      type: "oidc",
+      issuer: "https://id.twitch.tv/oauth2",
     }),
   ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.id = token.id;
+      session.accessToken = token.accessToken;
+      return session;
+    },
+    async jwt({ token, user, account, profile }) {
+      if (user) {
+        token.id = user.id;
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+  },
 });
