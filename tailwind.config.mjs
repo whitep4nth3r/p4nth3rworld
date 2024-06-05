f24c3be6786352bb39ffaaf130ea46ab849c1af8
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-06-05 12:56:13 +0100

    Add green hue to irradiated items

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  safelist: ["from-indigo-400", "from-emerald-400", "from-lime-400", "from-cyan-400", "from-yellow-400"],
+  safelist: [
+    "from-indigo-400",
+    "from-emerald-400",
+    "from-cyan-400",
+    "from-yellow-400",
+    "from-lime-400",
+    "from-lime-600",
+    "via-lime-800",
+  ],
   theme: {
     extend: {},
   },
