964d23a79a02208c234e759c47fdc7d40be63502
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-06-05 12:47:11 +0100

    Dynamic colours based on zones

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: ["from-indigo-400", "from-emerald-400", "from-lime-400", "from-cyan-400", "from-yellow-400"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
