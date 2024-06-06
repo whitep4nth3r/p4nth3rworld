648434d788a120d1af9fccf0c9692d519a15001d
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-06-06 11:46:39 +0100

    Add Sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import auth from "auth-astro";
 import netlify from "@astrojs/netlify";
-
 import tailwind from "@astrojs/tailwind";
+
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth(), tailwind()],
+  integrations: [
+    auth(),
+    tailwind(),
+    sentry({
+      dsn: import.meta.env.SENTRY_DSN,
+      debug: true,
+      sourceMapsUploadOptions: {
+        project: "p4nth3rworld",
+        authToken: import.meta.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
