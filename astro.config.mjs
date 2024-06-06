ed2d692c3e1e4199ce6e18c8a928073db8ec5497
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-06-06 12:39:33 +0100

    WILL IT WORK NOW?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,16 @@ import tailwind from "@astrojs/tailwind";
 
 import Sentry from "@sentry/astro";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     auth(),
     tailwind(),
     Sentry({
-      dsn: "https://4e15bfb05cdfab4664d7a35f9a660669@o4505635661873152.ingest.us.sentry.io/4507384490360832",
+      dsn: process.env.SENTRY_DSN,
       debug: true,
       sourceMapsUploadOptions: {
         project: "p4nth3rworld",
-        authToken: import.meta.env.SENTRY_AUTH_TOKEN,
+        authToken: process.env.SENTRY_AUTH_TOKEN,
       },
     }),
   ],
--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,7 +3,7 @@ import * as Sentry from "@sentry/astro";
 Sentry.init({
   dsn: "https://4e15bfb05cdfab4664d7a35f9a660669@o4505635661873152.ingest.us.sentry.io/4507384490360832",
   tracesSampleRate: 1.0,
-  debug: true,
+  debug: false,
   integrations: [Sentry.browserTracingIntegration()],
   tracePropagationTargets: ["https://p4nth3rb0t-mainframe.herokuapp.com", /\/world\//],
 });
