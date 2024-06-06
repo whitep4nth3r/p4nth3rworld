a18b38c6598b34a9be43f3b6e8931142bcaec265
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-06-06 12:40:44 +0100

    Change SENTRY_DSN to PUBLIC_SENTRY_DSN

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     auth(),
     tailwind(),
     Sentry({
-      dsn: process.env.SENTRY_DSN,
+      dsn: process.env.PUBLIC_SENTRY_DSN,
       debug: true,
       sourceMapsUploadOptions: {
         project: "p4nth3rworld",
