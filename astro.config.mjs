c5041575f174e906aa6b568fcc6ae0d718039b72
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-06-07 12:49:47 +0100

    Try deploying on Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import auth from "auth-astro";
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
-
 import Sentry from "@sentry/astro";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     auth(),
@@ -21,5 +22,5 @@ export default defineConfig({
     }),
   ],
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
 });
