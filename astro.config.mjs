00c959528731a96e3b215301348272f9b262a047
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-06-17 12:39:59 +0100

    Add tracesSampleRate to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
     Sentry({
       dsn: process.env.PUBLIC_ENV === "prod" ? process.env.PUBLIC_SENTRY_DSN : "",
       debug: true,
+      tracesSampleRate: 1.0,
       sourceMapsUploadOptions: {
         project: "p4nth3rworld",
         authToken: process.env.SENTRY_AUTH_TOKEN,
