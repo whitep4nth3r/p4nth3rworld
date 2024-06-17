d7e2261435fa2a2ee63c10913953f8ce0e843ce9
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-06-17 16:42:44 +0100

    Remove tracePropagationTargets from backend Sentry config

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -3,5 +3,4 @@ import * as Sentry from "@sentry/astro";
 Sentry.init({
   dsn: import.meta.env.PUBLIC_ENV === "prod" ? import.meta.env.PUBLIC_SENTRY_DSN : "",
   tracesSampleRate: 1.0,
-  tracePropagationTargets: ["https://p4nth3rb0t-mainframe.herokuapp.com", /\/world\//],
 });
