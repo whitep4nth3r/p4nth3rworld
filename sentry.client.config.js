d4d1e53364761f9278d5d7c1ed62b925435e54c4
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-06-17 12:37:49 +0100

    Debug true in client config

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,9 +3,9 @@ import * as Sentry from "@sentry/astro";
 Sentry.init({
   dsn: import.meta.env.PUBLIC_ENV === "prod" ? import.meta.env.PUBLIC_SENTRY_DSN : "",
   tracesSampleRate: 1.0,
-  debug: false,
+  debug: true,
   integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-  tracePropagationTargets: ["https://p4nth3rb0t-mainframe.herokuapp.com", /\/world\//],
+  tracePropagationTargets: [/https:\/\/p4nth3rb0t-mainframe.herokuapp\.com\/world/],
   replaysOnErrorSampleRate: 1.0,
   replaysSessionSampleRate: 1.0,
 });
