7ca63c53162dcf8e469e81bd3bcd2c61ecd8cd05
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-06-21 11:35:32 +0100

    Enable debug on server

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -4,4 +4,5 @@ Sentry.init({
   // dsn: import.meta.env.PUBLIC_SENTRY_DSN,
   dsn: import.meta.env.PUBLIC_ENV === "prod" ? import.meta.env.PUBLIC_SENTRY_DSN : "",
   tracesSampleRate: 1.0,
+  debug: true,
 });
